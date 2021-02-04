933f20ea6efcc6a6650d1a81c082d5db5a017a50
Lazzo1984 <stefano.lazzati1984@gmail.com>
2021-02-04 12:02:36 +0100

    Create Promote.groovy

---
+++ Promote.groovy
@@ -0,0 +1,22 @@
+JBOSS = "${JBOSS}"
+JBOSSWAR="${JBOSSWAR}"
+GIT="${GIT}"
+Ditrectory="${Directory}"
+Path="${Directory}\\${GIT}\\target"
+PathCondiviso="../../../Build"
+pipeline {
+    agent any
+        stages{
+        stage('Deploy application') { 
+        steps{
+         script{  
+                    sh "pwd"
+                    dir("${JENKINS_HOME}"){
+                    sh 'mkdir -p Build'
+                    sh 'cp ${Path}/${JBOSSWAR} ${PathCondiviso}'
+                    }
+                }
+            }
+        }
+    }
+}
